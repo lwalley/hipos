f9ae4f299be008790fd203d03801eecaad0875c9
Lisa Walley <lwalley@eol.org>
2013-11-10 22:30:52 -0500

    Switched to using font glyph for main menu icon.
    Added custom font set from icomoon with partial containing variables for each
    glyph; icomoon font is included in swimlanes theme and includes switch to using
    menu icon glyph instead of hamburger icon from png sprite.

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,9 +1,6 @@
 description %{ Styles for Drupal HPS distribution; styles are structured with
                SMACSS and BEM. }
 
-stylesheet 'sass/_init.scss',   :media => 'all'
-stylesheet 'sass/basic.scss',   :media => 'all'
-stylesheet 'sass/layout.scss',  :media => 'all'
-stylesheet 'sass/modules.scss', :media => 'all'
-stylesheet 'sass/theme.scss',   :media => 'all'
-stylesheet 'sass/print.scss',   :media => 'all'
+discover :stylesheets, :within => sass_dir, :media => 'all'
+discover :images,      :within => images_dir
+discover :fonts,       :within => fonts_dir
